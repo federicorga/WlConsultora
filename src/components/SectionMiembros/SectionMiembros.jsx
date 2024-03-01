8f48356a9203a8a3d77decebd06a8c1b5eb5b5e2
FedericoGre <federico.garea@gmail.com>
2024-03-01 15:28:04 -0300

    flexi3

--- src/components/SectionMiembros/SectionMiembros.jsx
+++ src/components/SectionMiembros/SectionMiembros.jsx
@@ -25,7 +25,9 @@ function SectionMiembros() {
       <Member nombre={"Wilhelm Leifermann "} titulo={"Fundador de WL Consultora. Abogado y Magíster en Política y Planificación del Transporte. Especializado en asesoramiento bursátil y asesoramiento legal a pequeñas y medianas empresas. A lo largo de su carrera profesional se ha desempeñado como apoderado de empresas privadas concesionarias de servicios públicos y consultor de emprendimientos comerciales. Complementa su formación profesional con el dominio de los idiomas inglés y portugués."} imgurl={"Willy.png"}/>
       <Member nombre={"Lucas Peña"} titulo={"Licenciado en Higiene y seguridad laboral - Medioambiente. A lo largo de su carrera ha auditado la ejecución de proyectos en caracter de responsable de area en obras publicas tanto para el Estado Nacional como para la Provincia de Buenos Aires. A su vez se ha desempeñado como asesor particular para emprendimientos comerciales."} imgurl={"LucasPena.png"}/>
       </div>
+      <div className='content-form-miembros'>
       <ContactoForm/>
+      </div>
       </section>
       </section>
       
