2422556b6b6ef275dac9abdb20ab715a353594d1
FedericoGre <federico.garea@gmail.com>
2024-03-03 20:31:28 -0300

    miembro modif

--- src/components/SectionMiembros/SectionMiembros.jsx
+++ src/components/SectionMiembros/SectionMiembros.jsx
@@ -1,7 +1,7 @@
 import './SectionMiembros.css'
 import LineSepar from '../ComponentesAccesorios/LineSepar/LineSepar';
 import Subtitleh2 from '../ComponentesAccesorios/Subtitles/Subtitleh2/Subtitleh2';
-//import ContactoForm from '../ContactoForm/ContactoForm';
+import ContactoForm from '../ContactoForm/ContactoForm';
 import PortadaSections from '../PortadaSections/PortadaSections';
 import Member from './Member/Member';
 
@@ -20,13 +20,13 @@ function SectionMiembros() {
         <p>Los miembros de WLConsultora son profesionales de diversas ramas del conocimiento orientados a la creación y puesta en marcha de empresas que cuentan con vasta experiencia en la materia, consolidada en la práctica diaria de resolución de cuestiones propias a la iniciativa de comenzar y ejercer emprendimientos comerciales.</p>
         <div className='content-form-miembros'>
 
-          <section className='Seccion-Content-Miembros'>
+         
               <div className='div-ContentAll-Miembros'>
               <Member nombre={"Wilhelm Leifermann "} titulo={"Fundador de WL Consultora. Abogado y Magíster en Política y Planificación del Transporte. Especializado en asesoramiento bursátil y asesoramiento legal a pequeñas y medianas empresas. A lo largo de su carrera profesional se ha desempeñado como apoderado de empresas privadas concesionarias de servicios públicos y consultor de emprendimientos comerciales. Complementa su formación profesional con el dominio de los idiomas inglés y portugués."} imgurl={"Willy.png"}/>
               <Member nombre={"Lucas Peña"} titulo={"Licenciado en Higiene y seguridad laboral - Medioambiente. A lo largo de su carrera ha auditado la ejecución de proyectos en caracter de responsable de area en obras publicas tanto para el Estado Nacional como para la Provincia de Buenos Aires. A su vez se ha desempeñado como asesor particular para emprendimientos comerciales."} imgurl={"LucasPena.png"}/>
               </div>
-          </section>
-         
+       
+          <ContactoForm/>
         </div>
       </section>
       
